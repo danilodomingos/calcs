c37e6e0ea7bca4f948d1a8d5ce45ad7e4d708990
Danilo Domingos <danilo.domingos@ifood.com.br>
2022-04-21 23:07:54 -0300

    feat: adicionando números primos.

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 const NumeroPrimo = require('./models/NumeroPrimo');
 
 const primo = new NumeroPrimo();
-
-
-console.log(primo.ehPrimo(1));
-console.log(primo.ehPrimo(2));
-console.log(primo.ehPrimo(0));
--- models/NumeroPrimo.js
+++ models/NumeroPrimo.js
@@ -6,6 +6,23 @@ class NumeroPrimo {
 
     ehPrimo(numero){
 
+        if(numero < 0 ){
+            numero  *= -1;
+        }
+
+        if(numero === 0 | numero === 1){
+            return false;
+        }
+
+        for(let valorAtual = 2; valorAtual < numero; valorAtual++){
+            
+            const resto = numero % valorAtual;
+
+            if(resto == 0){
+                return false;
+            }
+        }
+
         return true;
     }
 }
